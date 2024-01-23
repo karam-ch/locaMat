b21abf9ba5bde83b836492cb89600a56eebaeefc
kapnak <kapnak.mail@gmail.com>
2024-01-23 20:51:50 +0100

    Add migration files

---
+++ database/migrations/2024_01_23_193052_create_device_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('device', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->enum('type', ['TEL', 'PC', 'TAB']);
+            $table->string('ref', 5);
+            $table->string('version');
+            $table->binary('image')->nullable();
+            $table->char('tel', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('device');
+    }
+};
