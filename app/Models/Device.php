a26e9f24329007058ea3e7d0b5b3d60c699b495c
kapnak <kapnak.mail@gmail.com>
2024-01-24 11:35:40 +0100

    Update model (adding Borrow and adding methods to Device)

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -10,4 +10,14 @@ class Device extends Model
     use HasFactory;
 
     public $table = 'device';
+
+    public function borrows() {
+        return $this->hasMany(Borrow::class, 'device_id');
+    }
+
+    protected static function booted () {
+        static::deleting(function(Device $device) {
+            $device->borrows()->delete();
+        });
+    }
 }
